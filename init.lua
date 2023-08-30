b342e899d1f9c7a3cb029768e6cf17e8a702fd8e
Hannes Bibel <hannes.bibel@cgm.com>
2023-08-30 12:30:40 +0200

    Fix metals config not being passed

--- init.lua
+++ init.lua
@@ -531,17 +531,17 @@ cmp.setup {
 }
 
 -- Scala setup
+local metals_config = require("metals").bare_config()
+metals_config.init_options.statusBarProvider = "on"
+metals_config.on_attach = on_attach
 local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
 vim.api.nvim_create_autocmd("FileType", {
   pattern = { "scala", "sbt" },
   callback = function()
-    require("metals").initialize_or_attach({})
+    require("metals").initialize_or_attach(metals_config)
   end,
   group = nvim_metals_group,
 })
-local metals_config = require("metals").bare_config()
-metals_config.init_options.statusBarProvider = "on"
-metals_config.on_attach = on_attach
 
 vim.cmd.colorscheme "catppuccin"
 vim.opt.colorcolumn = "80"
