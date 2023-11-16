864a57b3f33856e0cfcbc15a557c5c593063e44a
Hannes Bibel <hannes.bibel@gmail.com>
2023-11-16 21:12:50 +0100

    Get some form of debugging config for Rust running

--- lua/custom/rust.lua
+++ lua/custom/rust.lua
@@ -2,6 +2,11 @@ local M = {}
 
 M.init = function()
   local rt = require("rust-tools")
+
+  -- https://morezerosthanones.com/posts/start_debugging_in_neovim/
+
+  local homedir = vim.env.HOME
+  local codelldb_install_dir = homedir .. "/.local/share/nvim/mason/packages/codelldb"
   rt.setup({
     server = {
       on_attach = function(_, bufnr)
@@ -14,7 +19,38 @@ M.init = function()
         require("custom.keymaps").lsp(bufnr)
       end,
     },
+    dap = {
+      adapter = {
+        type = "executable",
+        command = codelldb_install_dir .. "/codelldb",
+        name = "codelldb",
+      },
+    },
   })
+
+  -- run:
+  -- . $HOME/.local/share/nvim/mason/packages/codelldb/codelldb --port 13000
+  require("dap").adapters.codelldb = {
+    type = 'server',
+    host = '127.0.0.1',
+    port = 13000
+  }
+
+  require("dap").configurations.rust = {
+    {
+      name = "Debug",
+      type = "codelldb",
+      request = "launch",
+      program = function()
+        return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/target/debug/", "file")
+      end,
+      cwd = "${workspaceFolder}",
+      stopOnEntry = false,
+      args = {},
+      runInTerminal = false,
+      sourceLanguages = { 'rust' },
+    },
+  }
 end
 
 return M
