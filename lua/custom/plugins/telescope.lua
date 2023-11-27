4152da24dd83f143c6507b5c1a7745281feae8c2
Hannes Bibel <hannes.bibel@gmail.com>
2023-11-27 13:02:34 +0100

    Formatting

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -1,19 +1,19 @@
 local setup = function()
-	require('telescope').setup {
-		defaults = {
-			mappings = {
-				i = {
-					['<C-u>'] = false,
-					['<C-d>'] = false,
-				},
-			},
-			file_ignore_patterns = { 'node_modules', 'target', '.git' },
-		},
-	}
+  require('telescope').setup {
+    defaults = {
+      mappings = {
+        i = {
+          ['<C-u>'] = false,
+          ['<C-d>'] = false,
+        },
+      },
+      file_ignore_patterns = { 'node_modules', 'target', '.git/' },
+    },
+  }
 
-	pcall(require('telescope').load_extension, 'fzf')
+  pcall(require('telescope').load_extension, 'fzf')
 end
 
 return {
-	setup = setup,
+  setup = setup,
 }
