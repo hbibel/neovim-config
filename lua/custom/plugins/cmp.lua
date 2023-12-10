1613719d9a13a6f741aabe98acb6a5a3fe546dd4
Hannes Bibel <hannes.bibel@gmail.com>
2023-12-10 08:03:38 +0100

    Add snippet for Rust test blocks

--- lua/custom/plugins/cmp.lua
+++ lua/custom/plugins/cmp.lua
@@ -2,6 +2,7 @@ local setup = function()
   local cmp = require 'cmp'
   local luasnip = require 'luasnip'
   require('luasnip.loaders.from_vscode').lazy_load()
+  require("luasnip.loaders.from_snipmate").load()
   luasnip.config.setup {}
 
   cmp.setup {
