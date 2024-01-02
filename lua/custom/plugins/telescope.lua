bb40e38f34d8f9c8f5ef4ad3b5024fa5e20282c8
Hannes Bibel <hannes.bibel@gmail.com>
2024-01-02 09:32:45 +0100

    Configure Telescope to also search hidden files

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -1,17 +1,27 @@
 local setup = function()
-  require('telescope').setup {
+  require("telescope").setup {
     defaults = {
       mappings = {
         i = {
-          ['<C-u>'] = false,
-          ['<C-d>'] = false,
+          ["<C-u>"] = false,
+          ["<C-d>"] = false,
         },
       },
-      file_ignore_patterns = { 'node_modules', 'target', '.git/' },
+      file_ignore_patterns = { "node_modules", "target", ".git/" },
+      vimgrep_arguments = {
+        "rg",
+        "--color=never",
+        "--no-heading",
+        "--with-filename",
+        "--line-number",
+        "--column",
+        "--smart-case",
+        "--hidden",
+      },
     },
   }
 
-  pcall(require('telescope').load_extension, 'fzf')
+  pcall(require("telescope").load_extension, "fzf")
 end
 
 return {
