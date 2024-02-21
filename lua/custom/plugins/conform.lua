6cd9879f8b72b1f9d797d2c007fd75f6521721f5
Hannes Bibel <hannes.bibel@gmail.com>
2024-02-21 13:27:22 +0100

    Use ruff instead of black for Python formatting

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -1,6 +1,6 @@
 return {
   setup = function()
-    require('conform').setup({
+    require("conform").setup({
       format_on_save = {
         timeout_ms = 1500,
         lsp_fallback = true,
@@ -8,7 +8,7 @@ return {
       log_level = vim.log.levels.DEBUG,
       formatters_by_ft = {
         lua = { "stylua" },
-        python = { "isort", "black" },
+        python = { "isort", "ruff_format" },
         -- Scalafmt standalone is SLOOOOOOOOW, so we fallback to LSP formatting
         -- scala = { "scalafmt" },
         -- Go LSP formatting is fine for now, so no explicit config here
