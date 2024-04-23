76b2be18d44fa50ac38457b836e34f3f7646a3d8
Hannes Bibel <hannes.bibel@gmail.com>
2024-04-23 21:35:40 +0200

    Add support for xo linter

--- lua/custom/plugins/nvim-lint.lua
+++ lua/custom/plugins/nvim-lint.lua
@@ -17,9 +17,25 @@ return {
       }
     end
 
+    if vim.fn.filereadable("./node_modules/.bin/xo") then
+      -- Note: nvim-lint defines a function for cmd which determines whether
+      -- eslint is installed globally or in node_modules, but I don't install
+      -- it globally, so I don't need that.
+      nvim_lint.linters.eslint.cmd = vim.fn.fnamemodify('./node_modules/.bin/xo', ':p')
+      nvim_lint.linters.eslint.args = {
+        '--reporter',
+        'json',
+        '--stdin',
+        '--stdin-filename',
+        function() return vim.api.nvim_buf_get_name(0) end,
+      }
+    end
+
     nvim_lint.linters_by_ft = {
-      python = { "mypy", }
+      python = { "mypy", },
+      typescript = { "eslint", }
     }
+
     vim.api.nvim_create_autocmd({ "BufWritePost" }, {
       callback = function()
         require("lint").try_lint()
